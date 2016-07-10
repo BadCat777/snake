255e21f9f2d8387f4d911f578e9830c94fa52553
BadCat777 <kingarmor@yandex.ru>
2016-07-10 18:08:52 +0300

    Исправление pList 2 и отрисовка змейки

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -26,6 +26,9 @@ namespace Snake
 
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
+
+            Console.ReadLine();
             
         }
     }
