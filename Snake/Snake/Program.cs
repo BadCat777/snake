ee0d9f4378c1e84384886c7c5e13d938bfed303d
BadCat777 <kingarmor@yandex.ru>
2016-07-15 01:15:04 +0300

    Добавлены столкновения с препятствиями

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,17 +13,8 @@ namespace Snake
         {
             Console.SetBufferSize(80, 25);
 
-            //Отрисовка рамочки
-            HorizontalLine UpLine = new HorizontalLine(0, 78, 0, '#');
-            HorizontalLine DownLine = new HorizontalLine(0, 78, 24, '#');
-            VerticalLine LeftLine = new VerticalLine(0, 24, 0, '#');
-            VerticalLine RightLine = new VerticalLine(0, 24, 78, '#');
-            UpLine.Drow();
-            DownLine.Drow();
-            LeftLine.Drow();
-            RightLine.Drow();
-
-            //Отрисовка точек
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
@@ -35,6 +26,10 @@ namespace Snake
 
             while(true)
             {
+                if(walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
                 if(snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
